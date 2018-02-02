3869b03b3d16ba650421e3eac9450211c5ba6e17
Marko <marvel98@gmail.com>
2018-02-02 09:52:22 -0800

    change

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.set("view engine", "handlebars");
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
-    // useMongoClient: true
+    useMongoClient: true
 });
 
 var PORT = process.env.PORT || 3000;
